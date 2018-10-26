1a372d28a3d9ae0d6bc364bd859ab0bb9571d147
whentea <wentyyuniarti@gmail.com>
2018-10-26 16:45:48 +0700

    tambah about page

--- sample/urls.py
+++ sample/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 from sample import views
 
 
-urlpatterns = [url('^$', views.HomePageView.as_view())]
+urlpatterns = [
+    url('^$', views.HomePageView.as_view()),
+    url('^about/$', views.AboutPageView.as_view()), #about page
+]
